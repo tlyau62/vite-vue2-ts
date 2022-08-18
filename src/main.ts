24d0bc0cb986a136004bf537890a1ba2366cc4b5
Tsz Lam <tyautl@nexify.com.hk>
2022-08-18 22:35:52 +0800

    Add build library

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-import App from "./components/App.vue";
-import Vue from "vue";
-import "./style.scss";
-
-new Vue({
-  render(h) {
-    return h(App);
-  },
-}).$mount("#app");
+export { default as Button } from "./components/Button.vue";
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
+//@ts-check
 import { createVuePlugin } from "vite-plugin-vue2";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-export default {
+export default defineConfig({
   plugins: [
     createVuePlugin({
       jsx: true,
     }),
+    dts(),
   ],
-};
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/main.ts"),
+      name: "MyLib",
+      // the proper extensions will be added
+      fileName: "my-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
